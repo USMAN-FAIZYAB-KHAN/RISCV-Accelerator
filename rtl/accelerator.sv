2c4ece06f67df604366c6cfb7f337f0be52fcfdf
USMAN-FAIZYAB-KHAN <usmanfaizyab123@gmail.com>
2026-02-20 11:11:57 +0500

    Register map updated

--- rtl/accelerator.sv
+++ rtl/accelerator.sv
@@ -1,10 +1,10 @@
 module register_map (
-       	input logic clk, reg_write, 
-	input logic [5:0] read_reg1, read_reg2, write_reg,
-        input logic [31:0] write_data,
-        output logic [31:0] read_data1, read_data2
+    input logic clk, reg_write, 
+    input logic [5:0] read_reg1, read_reg2, write_reg,
+    input logic [31:0] write_data,
+    output logic [31:0] read_data1, read_data2
 );
-        logic [31:0] registers [0:47];
+    logic [31:0] registers [0:47];
        
 	always_ff @(posedge clk) begin
 		if (reg_write)
